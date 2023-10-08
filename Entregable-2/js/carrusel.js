c5a67e49088217ddedd2fea47937823f79b73d7b
florencia <fprat@alumnos.exa.unicen.edu.ar>
2023-10-08 19:58:32 -0300

    carrusel

--- Entregable-2/js/carrusel.js
+++ Entregable-2/js/carrusel.js
@@ -1,29 +1,76 @@
+
 "use strict";
 
-let cardWidth = 290;
-let seccionCarrousel = 0;
+let allBtnIzqs = document.querySelectorAll(".btn-left-cards");
+let allBtnDer = document.querySelectorAll(".btn-right-cards");
 
-function slideIzq(btnIzq) {
-    if(seccionCarrousel != 0) {
-        seccionCarrousel += 4;
-    } else {
-        seccionCarrousel = -8;
-    }
-    let slide = seccionCarrousel * cardWidth;
-    let aux = btnIzq.nextElementSibling;
-    let carrousel = aux.firstElementChild;
-    carrousel.style.transform = `translateX(${slide}px)`;
-}
 
-function slideDer(btnDer) {
-    if(seccionCarrousel >= -4) {
-        seccionCarrousel -= 4;
-    } else {
-        seccionCarrousel = 0;
-    }
-    let slide = seccionCarrousel * cardWidth;
-    let aux = btnDer.previousElementSibling;
-    let carrousel = aux.firstElementChild;
-    carrousel.style.transform = `translateX(${slide}px)`;
-}
-document.body.classList.add('no-scroll', 'preloader-active');
+allBtnIzqs.forEach(boton => {
+    boton.classList.add('hidden');
+});
+
+
+
+
+allBtnDer.forEach(boton => {
+    boton.addEventListener("click", function() {
+        let primerCincoCards =  boton.parentElement.firstElementChild;
+        let segundoCincoCards =  boton.parentElement.firstElementChild.nextElementSibling;
+    
+
+      
+          // Agrega las clases para la transformación al segundo elemento
+          primerCincoCards.classList.remove('transformacion-reset');
+          primerCincoCards.classList.remove('transformacion-izquierda');
+          segundoCincoCards.classList.remove('transformacion-izquierda');
+            primerCincoCards.classList.add('transformacion-derecha');
+            segundoCincoCards.classList.add('transformacion-derecha');
+            
+            //Quita la clase de transformación y agrega la clase de reset después de 1 segundo al segundo elemento
+            setTimeout(function() {
+
+            segundoCincoCards.classList.remove('transformacion-izquierda');
+            segundoCincoCards.classList.remove('transformacion-derecha');
+            segundoCincoCards.classList.remove('transformacion-reset');
+            segundoCincoCards.classList.add('transformacion-reset-dos');
+            
+            boton.previousElementSibling.classList.remove('hidden');
+            boton.classList.add('hidden');
+            }, 500);
+
+
+    });
+
+
+});
+
+
+allBtnIzqs.forEach(boton => {
+    boton.addEventListener("click", function() {
+        let primerCincoCards =  boton.parentElement.firstElementChild;
+        let segundoCincoCards =  boton.parentElement.firstElementChild.nextElementSibling;
+    
+
+       //Agrega las clases para la transformación al segundo elemento
+       primerCincoCards.classList.remove('transformacion-derecha');
+    segundoCincoCards.classList.remove('transformacion-derecha');
+    segundoCincoCards.classList.remove('transformacion-reset-dos');
+    primerCincoCards.classList.add('transformacion-izquierda'); 
+    segundoCincoCards.classList.add('transformacion-izquierda');
+  
+    setTimeout(function() {
+       // Quita la clase de transformación y agrega la clase de reset después de 1 segundo al segundo elemento
+       segundoCincoCards.classList.remove('transformacion-izquierda');
+      segundoCincoCards.classList.remove('transformacion-derecha');
+      primerCincoCards.classList.add('transformacion-reset');
+      
+      boton.nextElementSibling.classList.remove('hidden');
+     boton.classList.add('hidden');
+    }, 500);
+
+
+
+    });
+
+
+});
