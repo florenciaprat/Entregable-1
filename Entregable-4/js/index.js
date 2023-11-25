6819f0b2e7958280932c370a87d7eb7d92e7f904
florenciaprat <fprat@alumnos.exa.unicen.edu.ar>
2023-11-25 20:14:28 -0300

    el 11 hecho (ponele)

--- Entregable-4/js/index.js
+++ Entregable-4/js/index.js
@@ -13,29 +13,29 @@ let seccion = document.querySelector(".datos-juego");
 const seccionY = seccion.getBoundingClientRect().y; //pos vertical 
 let value = seccionY; //almaceno la pos vertical
 console.log(value) //imprimo
-if(value >= -200 && value <= 300){ //rangos de pos vertical y muestra o remueve las classlist
+if(value >= -100 && value <= 600){ //rangos de pos vertical y muestra o remueve las classlist
   
     game_1.classList.add("mostrar-imagen");
     game_2.classList.remove("mostrar-imagen");
 } 
 
-else if(value >= -800 && value <= -201){
+else if(value >= -700 && value <= -101){
     game_1.classList.remove("mostrar-imagen")
     game_2.classList.add("mostrar-imagen")
     game_3.classList.remove("mostrar-imagen")
 }
 
-else if(value >= -1200 && value <= -801){
+else if(value >= -1200 && value <= -701){
   game_2.classList.remove("mostrar-imagen")
   game_3.classList.add("mostrar-imagen")
   game_4.classList.remove("mostrar-imagen")
 }
 
-else if(value >= -1400 && value <= -1201){
+else if(value >= -2000 && value <= -1201){
   game_3.classList.remove("mostrar-imagen")
   game_4.classList.add("mostrar-imagen")
 }
-else if(value < -1401){
+else if(value < -2001){
   game_4.classList.remove("mostrar-imagen")
 }
 });
